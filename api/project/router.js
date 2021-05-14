fbaf0333f3643576917b585883fc890d07a9f625
Leah King <dancingheart714@yahoo.com>
2021-05-14 09:53:08 -0400

    routers wired

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
-// build your `/api/projects` router here
+const router = require('express').Router();
+
+router.use('*', (req, res, next) => {
+  res.json({ api: 'project router up' });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: 'something went wrong inside the project router',
+    message: err.message,
+    stack: err.stack,
+  });
+});
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,14 @@
-// build your `/api/resources` router here
+const router = require('express').Router();
+
+router.use('*', (req, res, next) => {
+  res.json({ api: 'resources router up' });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: 'something went wrong inside the resources router',
+    message: err.message,
+    stack: err.stack,
+  });
+});
+module.exports = router;
