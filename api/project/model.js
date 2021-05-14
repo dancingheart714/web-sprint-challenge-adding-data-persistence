99f3814d1cd743203b91aa785127bf3ea9768c83
Leah King <dancingheart714@yahoo.com>
2021-05-14 19:28:50 -0400

    got 12 passing

--- api/project/model.js
+++ api/project/model.js
@@ -1,7 +1,7 @@
 const db = require('../../data/dbConfig');
 
 const convertToBoolean = (project) => {
-  if (project.project_completed === 0 || !project.project_completed) {
+  if (project.project_completed === false || !project.project_completed) {
     return { ...project, project_completed: false };
   } else {
     return { ...project, project_completed: true };
@@ -20,9 +20,32 @@ function getById(id) {
   return db('projects').where('project_id', id).first();
 }
 
-async function createNewTask(task) {
-  const [task_id] = await db('projects').insert(convertToBoolean(task));
-  return getById(task_id);
+const result = (boolean) => {
+  if (boolean === true || boolean === 1 || boolean === '1') {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+async function createNewTask(project) {
+  const [id] = await db('projects').insert({
+    project_name: project.project_name,
+    project_description: project.project_description,
+    project_completed: result(project.project_completed),
+  });
+
+  let projects = await db('projects')
+    .select('*')
+    .where('project_id', id)
+    .first();
+
+  if (projects.project_completed === 1) {
+    projects.project_completed = true;
+  } else {
+    projects.project_completed = false;
+  }
+  return projects;
 }
 
 module.exports = {
